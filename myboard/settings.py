10dadc3c03e9da50a09eacb20ddaa80c52d5acf0
Wan1027 <rladhkstmd10@daum.net>
2022-08-13 19:23:52 +0900

    filtering

--- myboard/settings.py
+++ myboard/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -107,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS':[
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
 }
 
--- posts/views.py
+++ posts/views.py
@@ -3,14 +3,15 @@ from .models import Post
 from users.models import Profile
 from .serializers import PostSerializer, PostCreateSerializer
 from .permissions import CustomReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [CustomReadOnly]
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['author', 'likes']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author', 'likes']
 
     def get_serializer_class(self):
         if self.action == 'list' or 'retrieve':
